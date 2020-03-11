cfbbf87cffa1c66476ed419d4d2525f87a22e2e6
Ariane Holford <aribomop@gmail.com>
2020-03-11 15:34:55 +0000

    first commit

---
+++ pizza_controller.rb
@@ -0,0 +1,36 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative('models/pizza_order')
+also_reload('./models/*')
+
+get '/pizza_orders' do
+  @orders = PizzaOrder.all
+  erb(:index)
+end
+
+get '/pizza_orders/new' do
+  erb(:new)
+end
+
+get '/pizza_orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
+
+get '/pizza_orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+post '/pizza_orders' do
+  @order = PizzaOrder.new(params)
+  @order.save
+  erb(:create)
+end
+
+post '/pizza_orders/:id/delete' do
+  PizzaOrder.delete(params[:id])
+  redirect '/pizza_orders'
+end
